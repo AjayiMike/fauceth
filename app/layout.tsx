52062f964eddb31a60f0f8b323d6f73a9dba5aaa
AjayiMike <ajayimikeofficial@gmail.com>
2025-04-02 08:10:22 +0100

    feat: add AppQueryClientProvider to layout for React Query integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ConnectionProvider } from "@/providers/ConnectionProvider";
+import AppQueryClientProvider from "@/providers/QueryClient";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                <ConnectionProvider>{children}</ConnectionProvider>
+                <ConnectionProvider>
+                    <AppQueryClientProvider>{children}</AppQueryClientProvider>
+                </ConnectionProvider>
             </body>
         </html>
     );
