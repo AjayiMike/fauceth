ec9baf881c38aed8ea21aaf233ffa4bf9d5a537e
AjayiMike <ajayimikeofficial@gmail.com>
2025-05-12 06:54:00 +0100

    refactor: replace animated circles with a new path for the active icon in the NetworkDropdown component, improving visual representation

--- src/components/NetworkDropdown/StatusIcons.tsx
+++ src/components/NetworkDropdown/StatusIcons.tsx
@@ -4,72 +4,13 @@ const ActiveIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
         xmlns="http://www.w3.org/2000/svg"
         width="24"
         height="24"
-        viewBox="0 0 24 24"
+        viewBox="-5 -5 24 24"
         {...props}
     >
-        <circle cx="12" cy="12" r="0" fill="currentColor">
-            <animate
-                id="svgSpinnersPulse30"
-                fill="freeze"
-                attributeName="r"
-                begin="0;svgSpinnersPulse32.begin+0.4s"
-                calcMode="spline"
-                dur="1.2s"
-                keySplines=".52,.6,.25,.99"
-                values="0;11"
-            />
-            <animate
-                fill="freeze"
-                attributeName="opacity"
-                begin="0;svgSpinnersPulse32.begin+0.4s"
-                calcMode="spline"
-                dur="1.2s"
-                keySplines=".52,.6,.25,.99"
-                values="1;0"
-            />
-        </circle>
-        <circle cx="12" cy="12" r="0" fill="currentColor">
-            <animate
-                id="svgSpinnersPulse31"
-                fill="freeze"
-                attributeName="r"
-                begin="svgSpinnersPulse30.begin+0.4s"
-                calcMode="spline"
-                dur="1.2s"
-                keySplines=".52,.6,.25,.99"
-                values="0;11"
-            />
-            <animate
-                fill="freeze"
-                attributeName="opacity"
-                begin="svgSpinnersPulse30.begin+0.4s"
-                calcMode="spline"
-                dur="1.2s"
-                keySplines=".52,.6,.25,.99"
-                values="1;0"
-            />
-        </circle>
-        <circle cx="12" cy="12" r="0" fill="currentColor">
-            <animate
-                id="svgSpinnersPulse32"
-                fill="freeze"
-                attributeName="r"
-                begin="svgSpinnersPulse30.begin+0.8s"
-                calcMode="spline"
-                dur="1.2s"
-                keySplines=".52,.6,.25,.99"
-                values="0;11"
-            />
-            <animate
-                fill="freeze"
-                attributeName="opacity"
-                begin="svgSpinnersPulse30.begin+0.8s"
-                calcMode="spline"
-                dur="1.2s"
-                keySplines=".52,.6,.25,.99"
-                values="1;0"
-            />
-        </circle>
+        <path
+            fill="currentColor"
+            d="M1 10a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1m4-4a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V7a1 1 0 0 1 1-1m4-4a1 1 0 0 1 1 1v10a1 1 0 0 1-2 0V3a1 1 0 0 1 1-1m4-2a1 1 0 0 1 1 1v12a1 1 0 0 1-2 0V1a1 1 0 0 1 1-1"
+        />
     </svg>
 );
 
